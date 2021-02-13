ad85c7be1f242c4efdca680b3384a9903b7e9d99
Tomas <tosanchez@frba.utn.edu.ar>
2021-02-12 23:14:22 -0300

    feat: displaying correct buttons

--- src/components/HeaderActions/HeaderActions.jsx
+++ src/components/HeaderActions/HeaderActions.jsx
@@ -1,16 +1,32 @@
-import React, { useCallback, useState } from 'react';
+import React from 'react';
 import { Button } from '@ui5/webcomponents-react/lib/Button';
 import { useTranslation } from 'react-i18next';
 import { sapUiSmallMarginEnd } from '@ui5/webcomponents-react-base/lib/spacing';
+import { isMobile } from '@ui5/webcomponents-base/dist/Device';
 
+/**
+ * Split-Code for Obtainning corresponding buttons
+ * @memberof Detail
+ * @param {any} props the properties passed
+ * @private
+ */
 function HeaderActions(props) {
   const { t } = useTranslation();
 
-  return [
-    <Button key="1" icon="edit" design="Emphasized" tooltip={t('editButton')} style={sapUiSmallMarginEnd} />,
-    <Button key="2" icon="full-screen" design="Transparent" tooltip={t('fullscreenButton')} style={sapUiSmallMarginEnd} />,
-    <Button key="3" onClick={props.onClose} icon="decline" tooltip={t('closeButton')} design="Transparent" />,
-  ];
+  // Buttons to be displayed
+  var aButtons = [];
+
+  // Main action
+  aButtons.push(<Button key="1" icon="edit" design="Emphasized" tooltip={t('editButton')} style={sapUiSmallMarginEnd} />);
+
+  if (!isMobile())
+    // Maximize-Minimize (Not in phone for screen size)
+    aButtons.push(<Button key="2" icon={props.bFullScreen ? 'exit-full-screen' : 'full-screen'} design="Transparent" tooltip={t('fullscreenButton')} style={sapUiSmallMarginEnd} />);
+
+  // Close button
+  aButtons.push(<Button key="3" onClick={props.onClose} icon="decline" tooltip={t('closeButton')} design="Transparent" />);
+
+  return aButtons;
 }
 
 export default HeaderActions;
