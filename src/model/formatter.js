54ee9d75329d31ee4e4ea8f0e7115615be90fb40
Tomas <tosanchez@frba.utn.edu.ar>
2021-02-14 12:52:32 -0300

    feat(formatter): add new formatting functions

--- src/model/formatter.js
+++ src/model/formatter.js
@@ -1,7 +1,52 @@
 /**
- * Formats a string as a title
+ * Formatter container
+ * @method
+ * @public
+ */
+export const Formatter = {
+  toTitle: titleFormat,
+  toUser: userFormat,
+  toPaymentState: getPaymentState,
+};
+
+/**
+ * Obtains payment status value state
  * @function
  * @public
+ * @param {string} status the status letter
+ * @return {string} the status text
+ */
+export function getPaymentState(status = '') {
+  switch (status) {
+    case 'P':
+      return 'Success';
+    case 'E':
+      return 'Error';
+    case 'W':
+      return 'Warning';
+    case 'H':
+      return 'Information';
+    default:
+      return 'None';
+  }
+}
+
+/**
+ * Formats an ID to upper case.
+ * @function
+ * @public
+ * @param {string} usnam the user id
+ * @return {string}
+ */
+export function userFormat(usnam = '') {
+  return usnam != null ? usnam.toLocaleUpperCase() : 'UNKNOWN';
+}
+
+/**
+ * Formats a string as a title.
+ * @function
+ * @public
+ * @alias Formaterr~toTitle
  * @param {string} str the string to be formatted
  * @return {string} the formatted string
  */
