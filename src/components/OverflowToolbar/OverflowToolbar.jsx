0b9118686c0de864168e0ae1f250f424d9ea7973
Tomas <tosanchez@frba.utn.edu.ar>
2021-02-20 20:39:47 -0300

    ci: polymorfic overfloow toolbar

--- src/components/OverflowToolbar/OverflowToolbar.jsx
+++ src/components/OverflowToolbar/OverflowToolbar.jsx
@@ -11,28 +11,41 @@ import { Icon } from '@ui5/webcomponents-react';
 /**
  * Custom toolbar with searchfield
  * @function
- * @param {any} props
+ * @param {any} props using count for counter, onFilter for filter option, add for addition in toolbar
  * @returns {ui5.webcomponents.react.Toolbar} a custom Toolbar
  */
 const OverflowToolbar = (props) => {
   const { t } = useTranslation();
 
+  const { title = t('clientsListHeader'), count, onFilter, add = true, sort = false } = props;
+
   const onResetFilters = (oEvent) => {
     var oInput = oEvent.srcElement.parentNode.parentNode.childNodes[2].firstChild;
     oInput.value = '';
-    props.onFilter();
+    onFilter();
   };
+
+  const Content = [];
+  const oSearchField = (
+      <Input key="searchField" onInput={onFilter} placeholder={t('searchClientPH')} icon={<Icon name="search" interactive={false} showTooltip={true} tooltip={t('searchButton')} />} />
+    ),
+    oResetButton = <Button key="resetButton" icon="reset" design="Transparent" tooltip={t('resetButton')} onClick={onResetFilters} />,
+    oAddButton = <Button key="addButton" icon="add" design="Emphasized" tooltip={t('addButton')} />,
+    oSortButton = <Button key="sortButton" icon="sort" design="Transparent" tooltip={t('sortButton')} />;
+  if (onFilter) {
+    Content.push(oSearchField);
+    Content.push(oResetButton);
+  }
+  if (sort) Content.push(oSortButton);
+  if (add) Content.push(oAddButton);
+  if (Content.length > 0) Content.unshift(<ToolbarSpacer key="spacer" />);
 
   return (
     <Toolbar style={{ position: 'sticky' }}>
       <FlexBox>
-        <Title level={'H5'} style={{ marginLeft: '1rem' }}>{`${t('clientsListHeader')} (${props.count ? props.count : 0})`}</Title>
+        <Title level={'H5'} style={{ marginLeft: '1rem' }}>{`${title} (${count ? count : 0})`}</Title>
       </FlexBox>
-      <ToolbarSpacer />
-      <Input onInput={props.onFilter} placeholder={t('searchClientPH')} icon={<Icon name="search" interactive={false} showTooltip={true} tooltip={t('searchButton')} />} />
-      <Button icon="reset" design="Transparent" tooltip={t('resetButton')} onClick={onResetFilters} />
-      <Button icon="sort" design="Transparent" tooltip={t('sortButton')} />
-      <Button icon="add" design="Emphasized" tooltip={t('addButton')} />
+      {Content.length > 0 && Content.map((oItem) => oItem)}
     </Toolbar>
   );
 };
